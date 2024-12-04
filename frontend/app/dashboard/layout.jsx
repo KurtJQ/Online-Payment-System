5ab830ac16226b8b781fca84bf3384b3cd0f7184
KurtJQ <irockstar12230@gmail.com>
2024-12-04 23:58:45 +0800

    dashboard header

--- frontend/app/dashboard/layout.jsx
+++ frontend/app/dashboard/layout.jsx
@@ -3,21 +3,40 @@ import Image from "next/image";
 export default function DashboardLayout({ children }) {
   return (
     <>
-      <header>
-        <div>
+      <header className="bg-red-500 overflow-hidden p-3 px-12 flex justify-between">
+        <div className="inline-flex items-center">
           <Image
             src="/SCC icon.webp"
-            width={80}
-            height={80}
+            width={200}
+            height={200}
+            className="w-20 h-20 "
             alt="St Clare College Logo"
           />
-          <span>St. Clare College of Caloocan Inc.</span>
+          <span className="text-white text-2xl font-bold">
+            St. Clare College <br />
+            of Caloocan Inc.
+          </span>
         </div>
-        <div>
-          <button>
-            <img src="/bell-regular-48.png" alt="Notifications Button" />
+        <div className="inline-flex items-center gap-3">
+          <button className="bg-gray-300 rounded-full p-1">
+            <Image
+              src="/bell-regular-48.png"
+              width={48}
+              height={48}
+              alt="Notifications Button"
+            />
           </button>
-          <span>Kurt Justine Que</span>
+          <div className="inline-flex items-center bg-gray-300 rounded-full gap-1 px-4 py-1">
+            <div>
+              <Image
+                src="/user-circle-solid-48.png"
+                width={48}
+                height={48}
+                alt="Default Profile Picture"
+              />
+            </div>
+            <span className="text-2xl font-bold">KURT JUSTINE QUE</span>
+          </div>
         </div>
       </header>
       <nav></nav>
