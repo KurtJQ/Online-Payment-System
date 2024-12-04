304cfc71e1d2687197ec714c369ff8db6f29fc66
KurtJQ <irockstar12230@gmail.com>
2024-12-04 23:57:10 +0800

    tailwindcss configs

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx}"],
+  content: [
+    "./app/**/*.{js,jsx}",
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+  ],
   theme: {
     extend: {},
   },
