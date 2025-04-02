a813dd31c7eef2bc5986a2db3f577ff900f3fddc
KurtJQ <irockstar12230@gmail.com>
2025-04-03 03:04:36 +0800

    cleaned login

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Login } from "@/components/auth/sign-in";
+import { login } from "@/components/auth/sign-in";
 
 export default function Page() {
   return (
@@ -23,7 +23,55 @@ export default function Page() {
               alt="SCC Icon"
             />
           </div>
-          <Login />
+          <form action={login}>
+            <div className="mt-3 w-full">
+              <div>
+                <label htmlFor="email" className="hidden">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                  className="px-6 py-2 border-2 border-black w-full rounded-full"
+                />
+              </div>
+              <div className="mt-2">
+                <label htmlFor="password" className="hidden">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Password"
+                  className="px-6 py-2 border-2 border-black w-full rounded-full"
+                />
+              </div>
+            </div>
+            <div className="mt-2 flex justify-between">
+              <div>
+                <label htmlFor="rememberLogin" className="hidden">
+                  Remember Me
+                </label>
+                <input
+                  type="checkbox"
+                  htmlFor="rememberLogin"
+                  className="rounded-full"
+                  id="rememberLogin"
+                />
+                <span className="ml-2 font-bold">Remember Me</span>
+              </div>
+              <div>Forget Password?</div>
+            </div>
+            <button
+              type="submit"
+              className="bg-black text-white py-2 px-16 font-bold rounded-full float-right mt-4"
+            >
+              SIGN IN
+            </button>
+          </form>
         </div>
       </div>
     </>
