67c898cc81b002805b6a9dd746f8cf56906b14c4
KurtJQ <irockstar12230@gmail.com>
2025-04-04 03:33:44 +0800

    reverted try/catch in signIn function

--- frontend/components/auth/sign-in.jsx
+++ frontend/components/auth/sign-in.jsx
@@ -3,13 +3,9 @@
 import { signIn } from "@/app/auth";
 
 export async function login(formData) {
-  try {
-    const result = await signIn("credentials", {
-      email: formData.get("email"),
-      password: formData.get("password"),
-      redirectTo: "/dashboard",
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  await signIn("credentials", {
+    email: formData.get("email"),
+    password: formData.get("password"),
+    redirectTo: "/dashboard",
+  });
 }
