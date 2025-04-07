afde6bfed9ce0d097da85fbe85d5e5e2a6013b53
KurtJQ <irockstar12230@gmail.com>
2025-04-08 02:45:06 +0800

    transactionwidget now populates with data

--- frontend/components/dashboard/transactionwidget.jsx
+++ frontend/components/dashboard/transactionwidget.jsx
@@ -1,14 +1,41 @@
 import Link from "next/link";
-export default function transactionWidget() {
+import { auth } from "@/app/auth";
+
+export default async function transactionWidget() {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "PHP",
+  });
+  const session = await auth();
+  if (!session) {
+    return null;
+  }
+  const studentId = session.user._studentId;
+  let invoice;
+
+  try {
+    const res = await fetch(
+      `http://localhost:5050/api/student/invoice/${studentId}`
+    );
+    if (!res.ok) {
+      throw new Error(res.statusText);
+    }
+    invoice = await res.json();
+  } catch (error) {
+    console.error(error);
+  }
   return (
     <div className="bg-gray-300 rounded-3xl min-w-fit flex flex-col items-center p-3">
       {/* Header */}
       <div className="font-bold text-2xl">Recent Transactions</div>
       {/* Main Content */}
       <div className="overflow-y-auto basis-full">
-        <div className="bg-gray-500 text-white rounded-full p-2 text-sm">
-          2nd Periodical 1,500 PHP 11/14/2024
-        </div>
+        {invoice.map((invoice) => (
+          <div className="bg-gray-500 text-white rounded-full p-2 text-sm mb-2">
+            {new Date(invoice.createdAt).toLocaleDateString()}{" "}
+            {formatter.format(invoice.amount)}
+          </div>
+        ))}
       </div>
       {/* Footer */}
       <div className="ml-auto bg-gray-500 text-white p-1 px-2 rounded-full text-xs ">
