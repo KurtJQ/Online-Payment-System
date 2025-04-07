f7f01f97901621386d923e7cea13d142efabfbd2
KurtJQ <irockstar12230@gmail.com>
2025-04-08 02:32:34 +0800

    balance breakdown updated

--- frontend/components/dashboard/breakdown.jsx
+++ frontend/components/dashboard/breakdown.jsx
@@ -1,45 +1,82 @@
-export default function Fees({ hidePay }) {
+import { auth } from "@/app/auth";
+const session = await auth();
+
+async function getInfo() {
+  if (!session) {
+    return null;
+  }
+  const user = session.user._studentId;
+
+  try {
+    const res = await fetch(
+      `http://localhost:5050/api/student/profile-data/${user}`
+    );
+    if (!res.ok) {
+      throw new Error(res.statusText);
+    }
+    const info = await res.json();
+    return info[0];
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getInvoices(user) {
+  let invoices;
+  try {
+    const res = await fetch(
+      `http://localhost:5050/api/student/invoices/${user._studentId}/${user.yearLevel}/${user.schoolYear}`
+    );
+    if (!res.ok) {
+      throw new Error(res.statusText);
+    }
+    const invoices = await res.json();
+    return invoices;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function getCurrentPayments(invoices) {
+  let currentTotal = 0;
+  for (let i in invoices) {
+    currentTotal = currentTotal + invoices[i].amount;
+  }
+  return currentTotal;
+}
+
+export default async function Fees({ hidePay }) {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "PHP",
+  });
+  const total = 12000;
+  const user = await getInfo();
+  const invoices = await getInvoices(user);
+  const currentPayments = getCurrentPayments(invoices);
+  let balance = total - currentPayments;
+
   return (
     <div className="basis-full drop-shadow">
-      {/* Top */}
-      <div className="flex mb-4 justify-between text-2xl">
-        <div className="bg-gray-300 px-3 py-4 rounded-full font-bold">
-          Total Balance: 7,500 PHP
+      <div className="grid gap-3 bg-gray-300 rounded-3xl p-8 font-bold text-2xl ">
+        <div>Amount to pay</div>
+        <div className="flex border-b-2 pt-4 border-black">
+          Amount Currently Paid{" "}
+          <span className="ml-auto">{formatter.format(currentPayments)}</span>
+        </div>
+        <div className="flex border-b-2 pt-4 border-black">
+          Total Balance{" "}
+          <span className="ml-auto">{formatter.format(balance)}</span>
         </div>
         {hidePay ? (
           ""
         ) : (
-          <div className="bg-gray-300 px-3 py-4 rounded-full font-bold">
-            Pay Now
+          <div className="mt-8 flex">
+            <div className="bg-green-300 px-3 py-2 rounded-full font-semi ml-auto">
+              Pay Now
+            </div>
           </div>
         )}
-      </div>
-      {/* Bottom */}
-      <div className="grid gap-3 bg-gray-300 rounded-3xl p-8 font-bold text-2xl ">
-        <div className="flex border-b-2 border-black">
-          1st Periodical <span className="ml-auto">1,500 PHP</span>
-        </div>
-        <div className="flex border-b-2 border-black">
-          Prelim <span className="ml-auto">1,500 PHP</span>
-        </div>
-        <div className="flex border-b-2 border-black">
-          2nd Periodical <span className="ml-auto">1,500 PHP</span>
-        </div>
-        <div className="flex border-b-2 border-black">
-          Midterm <span className="ml-auto">1,500 PHP</span>
-        </div>
-        <div className="flex border-b-2 border-black">
-          3rd Periodical <span className="ml-auto">1,500 PHP</span>
-        </div>
-        <div className="flex border-b-2 border-black">
-          Pre-Finals <span className="ml-auto">1,500 PHP</span>
-        </div>
-        <div className="flex border-b-2 border-black">
-          4th Periodical <span className="ml-auto">1,500 PHP</span>
-        </div>
-        <div className="flex">
-          Finals <span className="ml-auto">1,500 PHP</span>
-        </div>
       </div>
     </div>
   );
