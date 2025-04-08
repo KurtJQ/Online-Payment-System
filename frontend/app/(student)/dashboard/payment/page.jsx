c5013e007d3f61beafeff7f514b198692650d342
KurtJQ <irockstar12230@gmail.com>
2025-04-08 17:49:07 +0800

    payment function implementation (wip)

--- frontend/app/(student)/dashboard/payment/page.jsx
+++ frontend/app/(student)/dashboard/payment/page.jsx
@@ -1,6 +1,50 @@
 import Fees from "components/dashboard/breakdown";
+import { auth } from "@/app/auth";
+const session = await auth();
 
-export default function Page() {
+function formatYear(year) {
+  switch (year) {
+    case "1":
+      return "1st Year";
+    case "2":
+      return "2nd Year";
+    case "3":
+      return "3rd Year";
+    case "4":
+      return "4th Year";
+    default:
+      return "ERROR Wrong format";
+  }
+}
+
+function formatSemester(semester) {
+  switch (semester) {
+    case "1":
+      return "First Semester";
+    case "2":
+      return "Second Semester";
+  }
+}
+
+export default async function Page() {
+  if (!session) {
+    return null;
+  }
+  const user = session.user;
+  let profile;
+
+  try {
+    const res = await fetch(
+      `http://localhost:5050/api/student/profile-data/${user._studentId}`
+    );
+    if (!res.ok) {
+      throw new Error(res.statusText);
+    }
+    profile = await res.json();
+  } catch (error) {
+    console.error(error);
+  }
+
   return (
     <div className="m-4 md:m-8 flex flex-col md:flex-row">
       {/* Fees Section */}
@@ -9,7 +53,7 @@ export default function Page() {
       </div>
 
       {/* Payment Section */}
-      <div className="bg-gray-300 rounded-3xl p-4 md:p-6 w-full md:w-2/3">
+      <div className="ml-4 bg-gray-300 rounded-3xl p-4 md:p-6 w-full md:w-2/3">
         <p className="font-medium">Select payment method</p>
         <div className="mt-3">
           <div className="border-2 border-black p-2 mb-2 rounded">
@@ -24,9 +68,11 @@ export default function Page() {
         </div>
         <div>
           <p className="font-medium">Payment for</p>
-          <p>Kurt Justine Que</p>
-          <p>BSCS</p>
-          <p>4th Year</p>
+          <p>
+            {profile.fname} {profile.mname} {profile.lname}
+          </p>
+          <p>{profile.course}</p>
+          <p>{formatYear(profile.yearLevel)}</p>
           <p>Second Semester</p>
         </div>
         <input
@@ -34,8 +80,13 @@ export default function Page() {
           placeholder="Enter amount"
           className="border-2 border-black p-2 mb-2 rounded w-full"
         />
-        <div className="bg-red-500 text-center text-white p-2 rounded-full font-bold">
-          <button type="submit">Complete payment</button>
+        <div>
+          <button
+            className="bg-red-500 text-center text-white p-2 rounded-full font-bold"
+            type="submit"
+          >
+            Complete payment
+          </button>
         </div>
       </div>
     </div>
