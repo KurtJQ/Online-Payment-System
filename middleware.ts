60a00e0167190ba649f6528f3e70b080c9584fb7
KurtJQ <irockstar12230@gmail.com>
2025-04-15 17:19:15 +0800

    middleware optimize

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,15 @@ import NextAuth from "next-auth";
 
 const { auth } = NextAuth(authConfig)
 export default auth(async function middleware(req){
-    if (!req.auth && req.nextUrl.pathname !== '/' && req.nextUrl.pathname !== '/signup'){
-        const newUrl = new URL('/', req.nextUrl.origin)
-        return Response.redirect(newUrl)
-    }
-})
+    const { pathname } = req.nextUrl; // Store path once
+  const isAuthRequired = !req.auth && pathname !== '/' && pathname !== '/signup';
+
+  if (isAuthRequired) {
+    const newUrl = new URL('/', req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
+});
+
 
 export const config = {
     matcher: ["/((?!api|_next/static|_next/image|images|favicon.ico).*)"]}
