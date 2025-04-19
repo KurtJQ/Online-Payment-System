f788b0a2fc6f61c4c7d2bc80343a1777e3eec0c4
KurtJQ <irockstar12230@gmail.com>
2025-04-20 00:35:08 +0800

    type added to Input function (pano nakalimutan to???)

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,13 +1,16 @@
 "use client";
 
-export default function Input({ label, value, onChange, name }) {
+export default function Input({ label, value, onChange, name, type }) {
   return (
     <div className="flex flex-col mb-4">
-      <label className="text-sm font-semibold text-gray-700 mb-1">{label}</label>
+      <label className="text-sm font-semibold text-gray-700 mb-1">
+        {label}
+      </label>
       <input
         value={value}
         onChange={onChange} // Bind to onChange for editable fields
         name={name}
+        type={type}
         className="p-2 rounded-xl border border-gray-300 bg-gray-100 text-gray-800 focus:outline-none"
       />
     </div>
