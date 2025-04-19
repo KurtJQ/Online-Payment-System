daf35dd8ca7929019174277285f0f365da94e0ce
KurtJQ <irockstar12230@gmail.com>
2025-04-20 01:12:46 +0800

    formatting

--- components/dashboard/getProfileData.js
+++ components/dashboard/getProfileData.js
@@ -1,18 +1,19 @@
 "use server";
 
-
 export async function getProfileData(studentId) {
-
   const baseURL = process.env.NEXT_PUBLIC_SERVER_URL;
 
   try {
-    const res = await fetch(`${baseURL}/api/student/profile-data/${studentId}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      cache: "no-store",
-    });
+    const res = await fetch(
+      `${baseURL}/api/student/profile-data/${studentId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        cache: "no-store",
+      }
+    );
 
     if (!res.ok) {
       throw new Error(`Failed to fetch: ${res.status}`);
