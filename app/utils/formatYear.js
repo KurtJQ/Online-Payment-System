8d6f57829aa29252d65916e5c47dacf044930b1e
KurtJQ <irockstar12230@gmail.com>
2025-05-09 20:44:44 +0800

    updated to match new types

--- app/utils/formatYear.js
+++ app/utils/formatYear.js
@@ -1,12 +1,12 @@
 export default function formatYear(year) {
   switch (year) {
-    case "1":
+    case 1:
       return "1st Year";
-    case "2":
+    case 2:
       return "2nd Year";
-    case "3":
+    case 3:
       return "3rd Year";
-    case "4":
+    case 4:
       return "4th Year";
     default:
       return "ERROR Wrong format";
