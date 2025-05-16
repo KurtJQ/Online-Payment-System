13a27938d2920bb0115215f24cf42d64fb1bc32d
KurtJQ <irockstar12230@gmail.com>
2025-05-16 20:36:40 +0800

    added PSA to documents

--- components/dashboard/DocumentSubmission.jsx
+++ components/dashboard/DocumentSubmission.jsx
@@ -39,46 +39,91 @@ export function DocumentSubmission({ student }) {
     }
   };
   return (
-    <div className="bg-gray-300 rounded-lg p-6 ">
-      {student.files.find((data) => data.doctype === "form137") ? (
-        (() => {
-          const file = student.files.find((data) => data.doctype === "form137");
-          const fileUrl = `data:${file.mimeType};base64,${file.base64}`;
+    <div className="bg-gray-300 rounded-3xl p-6 space-y-3">
+      <div>
+        {student.files.find((data) => data.doctype === "form137") ? (
+          (() => {
+            const file = student.files.find(
+              (data) => data.doctype === "form137"
+            );
+            const fileUrl = `data:${file.mimeType};base64,${file.base64}`;
 
-          return (
-            <div className="space-y-2">
-              <p className="font-semibold">Form 137 Uploaded:</p>
-              <a
-                href={fileUrl}
-                download={file.filename}
-                className="text-blue-600 underline text-sm"
-              >
-                Download File
-              </a>
-            </div>
-          );
-        })()
-      ) : (
-        <form onSubmit={handleSubmit}>
-          <label>
-            Form 137
-            <input type="file" className="mb-3" name="file" />
-          </label>
-          <input
-            type="text"
-            className="hidden"
-            name="document"
-            readOnly
-            value="form137"
-          />
-          <button
-            type="submit"
-            className="bg-red-500 text-white font-semibold rounded-lg p-2"
-          >
-            Confirm
-          </button>
-        </form>
-      )}
+            return (
+              <div className="space-y-2">
+                <p className="font-semibold">Form 137 Uploaded:</p>
+                <a
+                  href={fileUrl}
+                  download={file.filename}
+                  className="text-blue-600 underline text-sm"
+                >
+                  Download File
+                </a>
+              </div>
+            );
+          })()
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <label>
+              Form 137
+              <input type="file" className="mb-3" name="file" />
+            </label>
+            <input
+              type="text"
+              className="hidden"
+              name="document"
+              readOnly
+              value="form137"
+            />
+            <button
+              type="submit"
+              className="bg-red-500 text-white font-semibold rounded-lg p-2"
+            >
+              Confirm
+            </button>
+          </form>
+        )}
+      </div>
+      <div>
+        {student.files.find((data) => data.doctype === "psa") ? (
+          (() => {
+            const file = student.files.find((data) => data.doctype === "psa");
+            const fileUrl = `data:${file.mimeType};base64,${file.base64}`;
+
+            return (
+              <div className="space-y-2">
+                <p className="font-semibold">Form 137 Uploaded:</p>
+                <a
+                  href={fileUrl}
+                  download={file.filename}
+                  className="text-blue-600 underline text-sm"
+                >
+                  Download File
+                </a>
+              </div>
+            );
+          })()
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <label>
+              PSA
+              <input type="file" className="mb-3" name="file" />
+            </label>
+            <input
+              type="text"
+              className="hidden"
+              name="document"
+              readOnly
+              value="psa"
+            />
+            <button
+              type="submit"
+              className="bg-red-500 text-white font-semibold rounded-lg p-2"
+            >
+              Confirm
+            </button>
+          </form>
+        )}
+      </div>
     </div>
   );
 }
